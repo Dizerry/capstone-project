020f75177d6b5782ebd69eb146b367df49ca9bf8
enu enwe dizzerry <enwedizzerryenu107@gmail.com>
2025-01-21 21:01:12 +0400

    this is a test

---
+++ capstoneproject/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is a project file
+    </div>
+  );
+}
+
+export default App;
